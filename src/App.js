fc09fa617e756d4e6075ddc27006ce2045254316
Sujin Lee <sujinbonnaud@gmail.com>
2022-09-05 12:23:53 -0500

    add search box with function

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log('constructor');
   }
 
   componentDidMount() {
+    console.log('componentDidMount');
      fetch('https://jsonplaceholder.typicode.com/users')
      .then((response) => response.json())
      .then((user) => this.setState(() => {
@@ -24,21 +26,35 @@ class App extends Component {
   }
 
   render() {
+    console.log('render');
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => {
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={(event) => {
+            console.log(event.target.value); 
+            const searchString = event.target.value.toLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return {monsters: filteredMonsters}
+            })
+          }}
+        />
+        {this.state.monsters.map((monster) => {
             return (
               <div key={monster.id}>
                 <h1>{monster.name}</h1>
               </div>
             );
-          })
-        }
+        })}
       </div>
     );
   }
-  
 }
 
 export default App;
